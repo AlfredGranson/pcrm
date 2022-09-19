96bc409b9e78d463de89f834e71ac22a97c8ec04
Alfred Granson <alfred.granson@movehq.com>
2022-09-19 01:11:49 -0400

    fix: Adding locales

--- config/config.exs
+++ config/config.exs
@@ -50,3 +50,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :pcrm, PcrmWeb.Gettext,
+  default_locale: "en",
+  locales: ~w(en es)
