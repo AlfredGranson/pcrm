2ceef8b4874ee298647fabb696030eb436f6e122
Alfred Granson <alfred.granson@movehq.com>
2022-10-06 22:32:30 -0400

    feat: Add sweetalert2 to make confirms more consistant

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,15 +6,9 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import topbar from "topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
-
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -24,4 +18,49 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+// Show progress bar on live navigation and form submits
+import topbar from "topbar"
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+// Override the default notifications
+import Swal from 'sweetalert2'
+document.body.addEventListener('phoenix.link.click', function (e) {
+  e.stopPropagation();
+
+  var message = e.target.getAttribute("data-confirm");
+  if(message){
+    e.preventDefault();
+
+    Swal.fire({
+      html: message,
+      showCancelButton: true,
+      focusConfirm: true,
+      confirmButtonText:'Ok',
+      cancelButtonText: "cancel",
+      buttonsStyling: false,
+      reverseButtons: true,
+      focusConfirm: false,
+      showClass: {
+        popup: 'fade-in-scale'
+      },
+      customClass: {
+        container: "cu-pointer",
+        popup: "br2 cu-default",
+        htmlContainer: "dark-gray",
+        confirmButton: ["b--black-20","bn","bg-animate","bg-blue","br2","button-reset","dib","f6","fw6","hover-bg-dark-blue","mb3","no-underline",".outline-0", "ph3","ph4-l","pointer","pv2","pv3-l","white"],
+        cancelButton: ["b--black-20","bn","bg-animate","bg-silver","br2","button-reset","dib","f6","fw6","hover-bg-gray","mb3","mr3","no-underline",".outline-0","ph3","ph4-l","pointer","pv2","pv3-l","white"]
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        e.target.removeAttribute("data-confirm");
+        e.target.click();
+        e.target.click();
+      }
+    });
+  }
+
+}, false);
 
