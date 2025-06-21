0b3d3046b7c9482ff60a871e2e4cd9688b6f0df6
Alfred Granson <alfredg@gmail.com>
2025-06-20 21:21:39 -0600

    fix: Removing unused call

--- lib/pcrm_web/live_locale.ex
+++ lib/pcrm_web/live_locale.ex
@@ -1,6 +1,4 @@
 defmodule PcrmWeb.LiveLocale do
-  import Phoenix.LiveView
-
   def on_mount(:default, _params, %{"locale" => locale} = _session, socket) do
     Gettext.put_locale(PcrmWeb.Gettext, locale)
 
